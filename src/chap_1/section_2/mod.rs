340fb4db16d31c4ff3a0110b0b908218294db57f
Zhongren Shao <shao.zhongren@gmail.com>
2021-06-15 22:55:00 -0500

    change to trait

--- src/chap_1/section_2/mod.rs
+++ src/chap_1/section_2/mod.rs
@@ -1,35 +1,39 @@
 use num::complex::Complex;
 use std::fmt::Display;
 
-struct BasicQBit {
+pub trait Qubit {
+    fn verify_total_probability(&self) -> bool;
+}
+
+struct RealQubit {
     alpha: f64,
     beta: f64,
 }
 
-impl BasicQBit {
+impl Qubit for RealQubit {
     fn verify_total_probability(&self) -> bool {
         self.alpha.powi(2) + self.beta.powi(2) == 1.0
     }
 }
 
-impl Display for BasicQBit {
+impl Display for RealQubit {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         write!(f, "{}|0> + {}|1>", self.alpha, self.beta)
     }
 }
 
-struct QBit {
+struct FullQubit {
     alpha: Complex<f64>,
     beta: Complex<f64>,
 }
 
-impl QBit {
+impl Qubit for FullQubit {
     fn verify_total_probability(&self) -> bool {
         self.alpha.powi(2) + self.beta.powi(2) == Complex::new(1.0, 0.0)
     }
 }
 
-impl Display for QBit {
+impl Display for FullQubit {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         write!(f, "{}|0> + {}|1>", self.alpha, self.beta)
     }
