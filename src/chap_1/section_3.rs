44188bd10521f491e8237d6253159761940b05e3
Zhongren Shao <shao.zhongren@gmail.com>
2021-06-20 00:23:30 -0500

    using one and zero functions instead

--- src/chap_1/section_3.rs
+++ src/chap_1/section_3.rs
@@ -1,15 +1,15 @@
-use nalgebra::Matrix2;
-use num::complex::Complex;
+use nalgebra::{Matrix2, Matrix4, Vector4};
+use num::{complex::Complex, One, Zero};
 
 use super::section_2::QubitVector;
 
 pub fn x_gate(qubit: QubitVector) -> QubitVector {
     // a|0> + b|1> -> b|0> + a|1>
     let mat = Matrix2::new(
-        Complex::new(0.0, 0.0),
-        Complex::new(1.0, 0.0),
-        Complex::new(1.0, 0.0),
-        Complex::new(0.0, 0.0),
+        Complex::zero(),
+        Complex::one(),
+        Complex::one(),
+        Complex::zero(),
     );
     mat * qubit
 }
@@ -17,10 +17,10 @@ pub fn x_gate(qubit: QubitVector) -> QubitVector {
 pub fn z_gate(qubit: QubitVector) -> QubitVector {
     // a|0> + b|1> -> a|0> - b|1>
     let mat = Matrix2::new(
-        Complex::new(1.0, 0.0),
-        Complex::new(0.0, 0.0),
-        Complex::new(0.0, 0.0),
-        Complex::new(-1.0, 0.0),
+        Complex::one(),
+        Complex::zero(),
+        Complex::zero(),
+        -Complex::one(),
     );
     mat * qubit
 }
@@ -43,3 +43,30 @@ pub fn h_gate(qubit: QubitVector) -> QubitVector {
 // and
 // | e^(-ib/2), 0 |
 // | 0, e^(ib/2)  |, which is a rotation about the z axis
+
+pub fn cnot_gate(qubit1: QubitVector, qubit2: QubitVector) -> (QubitVector, QubitVector) {
+    let combined = Vector4::new(qubit1.x, qubit1.y, qubit2.x, qubit2.y);
+    let mat = Matrix4::new(
+        Complex::new(1.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(1.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(1.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0),
+        Complex::new(1.0, 0.0),
+        Complex::new(0.0, 0.0),
+    );
+    let result = mat * combined;
+    (
+        QubitVector::new(result.w, result.x),
+        QubitVector::new(result.y, result.z),
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use chap_1::section_2::QubitVector;
 use chap_1::section_3::{h_gate, x_gate, z_gate};
 use num::Complex;
 
+use crate::chap_1::section_3::cnot_gate;
+
 fn main() {
     let complex = QubitVector::new(Complex::new(0.0, 0.0), Complex::new(1.0, 0.0));
     println!("{}", complex);
@@ -11,4 +13,9 @@ fn main() {
     println!("{}", z_gate(complex));
     println!("{}", h_gate(complex));
     println!("{}", h_gate(h_gate(complex)));
+    let a = QubitVector::new(Complex::new(1.0, 0.0), Complex::new(0.0, 0.0));
+    let b = QubitVector::new(Complex::new(0.0, 0.0), Complex::new(1.0, 0.0));
+    let (x, y) = cnot_gate(a, b);
+    println!("{}", x);
+    println!("{}", y);
 }
