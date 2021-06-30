f90ca2edfd4b1e485d1167a3999fd1b6f4bb2637
Zhongren Shao <shao.zhongren@gmail.com>
2021-06-29 23:20:32 -0500

    added toffoli truth table

--- src/chap_1/section_4.rs
+++ src/chap_1/section_4.rs
@@ -1,1 +1,34 @@
+pub fn section_4() {
+    classical_computations();
+}
 
+fn classical_computations() {
+    let permutations = vec![
+        (false, false, false),
+        (false, false, true),
+        (false, true, false),
+        (false, true, true),
+        (true, false, false),
+        (true, false, true),
+        (true, true, false),
+        (true, true, true),
+    ];
+
+    for (a, b, c) in permutations {
+        println!(
+            "{}, {}, {} -> {}",
+            a as u8,
+            b as u8,
+            c as u8,
+            classical_toffoli_gate(a, b, c) as u8
+        );
+    }
+}
+
+pub fn classical_toffoli_gate(a: bool, b: bool, c: bool) -> bool {
+    if a && b {
+        !c
+    } else {
+        c
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use chap_1::section_3::section_3;
+use chap_1::{section_3::section_3, section_4::section_4};
 
 mod chap_1;
 
 fn main() {
-    section_3()
+    section_3();
+    section_4();
 }
