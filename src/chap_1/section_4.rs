2bb4d2f76843155dcdcccad51f37cf6a7de31db8
Zhongren Shao <shao.zhongren@gmail.com>
2021-07-08 22:14:59 -0500

    added comments for Deutsch's algorithm

--- src/chap_1/section_4.rs
+++ src/chap_1/section_4.rs
@@ -35,3 +35,16 @@ pub fn classical_toffoli_gate(a: bool, b: bool, c: bool) -> bool {
 
 // hadamard transform is when you have n parallel Hadamard gates operating on n qubits
 // transforms each qubit into a superposition of |0> and |1>
+// results in 1/sqrt(2^n)*sum(|x>)
+// to calculate the output of all values of x, we can do 1/sqrt(2^n)*sum(|x>|f(x)>)
+
+// a QNOT b == (a + b) % 2
+// Deutsch's algorithm
+// |v0> = |01>
+// |v1> = [(|0> + |1>)/sqrt(2)][(|0> - |1>)/sqrt(2)]
+// |v2> = +-[(|0> + |1>)/sqrt(2)][(|0> - |1>)/sqrt(2)] if f(0) == f(1)
+//        +-[(|0> - |1>)/sqrt(2)][(|0> - |1>)/sqrt(2)] if f(0) != f(1)
+// |v3> = +-|0>[(|0> - |1>)/sqrt(2)] if f(0) == f(1)
+//        +-|1>[(|0> - |1>)/sqrt(2)] if f(0) != f(1)
+//      = +-|f(0) QNOT f(1)>[(|0> - |1>)/sqrt(2)]
+// thus, we can measure the first qubit and get information about the outputs of the function for both values of x
